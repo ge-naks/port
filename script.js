f9d9d6d4e0b55a36019d4f42d56191b554afc7b4
ge-naks <georgewnakhla@gmail.com>
2024-01-02 20:50:08 -0500

    v1
    completed. ready for revisions

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        // Slick Carousel options and settings go here
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false
+        // Add more options as needed
+    });
+});
